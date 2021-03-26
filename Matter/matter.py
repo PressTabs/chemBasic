377dd60d93d6e4374408bc33dece6e7282ea2bac
PressTabs <thomastabsbrooks@gmail.com>
2021-03-26 12:16:40 -0700

    Added states to matter

--- Matter/matter.py
+++ Matter/matter.py
@@ -2,7 +2,7 @@ from Chemistry_Units import chem_unit
 
 
 class Matter:
-    def __init__(self, mass, volume=None):
+    def __init__(self, mass, volume=None, state=None):
         if isinstance(mass, chem_unit.ChemUnit):
             self.mass = mass
         else:
@@ -15,7 +15,15 @@ class Matter:
             volume_comp = volume.split(' ')
             self.volume = chem_unit.ChemUnit(volume_comp[0], volume_comp[1])
 
+        if {'solid': 0, 'liquid': 1, 'gas': 2, 'be': 3, 'plasma': 4}.get(state) != -1:
+            self.state = state
+
     def is_volume_defined(self):
         if hasattr(self, 'volume'):
+            return True
+        return False
+
+    def is_state_defined(self):
+        if hasattr(self, 'state'):
             return True
         return False
